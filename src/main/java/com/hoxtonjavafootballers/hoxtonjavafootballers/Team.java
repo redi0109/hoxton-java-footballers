47b24c4e04e177776708d5ab5ea219fc823c86a8
Demir Boraj <demirboraj97@gmail.com>
2022-10-07 01:10:15 +0200

    1

--- src/main/java/com/hoxtonjavafootballers/hoxtonjavafootballers/Team.java
+++ src/main/java/com/hoxtonjavafootballers/hoxtonjavafootballers/Team.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -32,6 +34,11 @@ class TeamController{
     @GetMapping("/teams")
     public List<Team> getAllTeams() {
         return teamRepo.findAll();
+    }
+
+    @PostMapping("/teams")
+    public Team createTeam(@RequestBody Team teamData) {
+        return teamRepo.save(teamData);
     }
 }
 
