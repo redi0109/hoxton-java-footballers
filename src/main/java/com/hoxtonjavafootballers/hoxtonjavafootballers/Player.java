2301eed65ee4f99f9c083c94d273f160a8c90a25
Demir Boraj <demirboraj97@gmail.com>
2022-10-07 01:16:03 +0200

    1

--- src/main/java/com/hoxtonjavafootballers/hoxtonjavafootballers/Player.java
+++ src/main/java/com/hoxtonjavafootballers/hoxtonjavafootballers/Player.java
@@ -1,8 +1,17 @@
 package com.hoxtonjavafootballers.hoxtonjavafootballers;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Entity
 public class Player {
@@ -18,3 +27,22 @@ public class Player {
         
     }
 }
+
+@RestController
+class PlayerController{
+
+    @Autowired
+    private PlayerRepo playerRepo;
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return playerRepo.findAll();
+    }
+
+    @PostMapping("/players")
+    public Player createTeam(@RequestBody Player playerData) {
+        return playerRepo.save(playerData);
+    }
+}
+
+interface PlayerRepo extends JpaRepository<Player, Integer> {}
